9b87681f9f9e5c70e67a6277e8283bd7aa2dd3f2
DalVlad <vladbulanov47@gmail.com>
2026-06-10 14:13:37 +0300

    init

---
+++ api-gateway/src/main/java/vlad/way/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package vlad.way.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ user-service/src/test/java/vlad/way/user/service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.way.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
